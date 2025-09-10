6b5582f795e4247c1e278e423b9397d38c1a291a
vitor2008 <Vitor_tonello@gmail.com>
2025-09-09 22:15:16 -0300

    variaveis em javascript

---
+++ logica js/variaveis.js
@@ -0,0 +1,25 @@
+//var nome = "Vitor"//string
+
+
+//console.log(nome)
+
+// variavel com let
+// let nome = "Vitor"
+ // nome = "Hugo"
+
+ const nome = "Vitor"
+ const sobrenome = "Tonello"
+ const idade = 42 //number
+const masculino = true // boolean
+
+//array
+const frutas = ["laranja", "maçã", "banana"]
+
+console.log(frutas[0])
+
+ //console.log("Meu nome é "+ nome +" " + sobrenome + " e minha idade é " + idade + " anos")
+
+
+
+ console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
+
