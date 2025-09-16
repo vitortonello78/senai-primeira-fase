00a309762fef4918d4a4e16eddcf8ca3b462ad41
VITOR HUGO TONELLO <vitor_tonello@estudante.sesisenai.org.br>
2025-09-15 21:58:57 -0300

    github

---
+++ logica js/script2.js
@@ -0,0 +1,23 @@
+//alert("Olá Mundo!")
+//alert("Oi")
+//alert("Tudo Bem?")
+//alert("Voce vem sempre aqui?")
+
+let idade = 18
+alert(idade)
+
+idade = "dezoito"
+alert(idade)
+
+idade = 18.1
+alert(idade)
+
+const amor = "meu primeiro amor"
+alert(amor)
+
+alert(amor)
+const amor2  = "meu segundo amor"
+
+var antigao = "Nunca usar essa poioca"
+
+
