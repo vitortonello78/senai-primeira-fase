4a221418420055b71672b9caacf9b2d4e4434df0
vitor2008 <Vitor_tonello@gmail.com>
2025-09-29 22:16:16 -0300

    while

---
+++ logica js/repetições/script.js
@@ -0,0 +1,15 @@
+let acertos = Number(prompt("Quantos acertos?:"))
+let valorPorQuestão = 10/16
+let nota = acertos * valorPorQuestão
+
+alert("sua nota foi " + nota)
+
+
+
+let contador = 0
+while(contador < 5){
+    alert("contador:" + contador)
+    contador = contador + 1
+    console.log("Contador: " + contador)
+
+}
