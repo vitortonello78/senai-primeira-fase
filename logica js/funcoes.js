d281fea0063ce27394f24f5366ec2eb5849150be
vitor2008 <Vitor_tonello@gmail.com>
2025-10-06 20:08:18 -0300

    Exercicios.js

---
+++ logica js/funcoes.js
@@ -0,0 +1,17 @@
+//  function saudação(nome){
+ //   console.log(`Bom dia ${nome}!`)
+// }
+
+// const saudação = (nome) =>{
+ //   console.log(`Bom dia ${nome}!`)
+// }
+
+// saudação("João")
+// saudação("Maria")
+
+const soma = (x,y) => x+y
+console.log(`soma: ${soma(10,5)}`)
+
+const multiplicação = (x,y) => x+y
+// console.log(`soma: ${soma(10,5)}`)
+
---
+++ logica js/repetições/for.js
@@ -0,0 +1,33 @@
+
+         //   let i = 0
+        //    while(i<3){
+         //       alert("Valor do i: " + i)
+        //        i = i + 1
+           //    i++
+            // }
+
+              //  for(let i = 0; i<3; i++){
+                //    alert("Valor do i: " + i)
+        
+           // }
+
+             //   let soma = 0
+             //   let nota1 = Number(prompt("Digite a nota: "))
+             //   soma = nota1
+             //   let nota2 = Number(prompt("Digite a nota 2: "))
+              //  soma = soma + nota2
+             //   let nota3 = Number(prompt("Digite a nota 3: "))
+              //  soma = soma + nota3
+
+  // for(let i = 0; i<10; i++){
+  //  let nota = Number(prompt("digite a "+ (i+1) + "a nota:"))
+  //   soma = soma + nota
+// }
+
+alert("soma: " + soma)
+
+const nomes =  ["Dona bete", "Mano juca","Lucio","Fernando", "Capitão Ganso"]
+
+for(let i=0; i<4; i++){
+    alert(nomes[i])
+}
