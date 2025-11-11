986958b3e402a55d1d179c9fa794f920c606e4c7
vitor2008 <Vitor_tonello@gmail.com>
2025-11-10 21:54:34 -0300

    trabalho do curso

--- dom-manipulacao/CRUD/script.js
+++ dom-manipulacao/CRUD/script.js
@@ -1,34 +1,77 @@
 
- const dinossauro = {
- nome: `Rex`,
- altura: `6`
-}
+// teste crud
+
+// const dinossauro = {
+//     nome: 'Rex',
+//     altura: '6'
+// }
 
 const dinossauros = []
+// let dinossauros = []
 
 // dinossauros.push(dinossauro)
-// document.getElementById("Dino").innerHTML = `nome: ${dinossauros[0].nome} - altura: ${dinossauros[0].altura}`
 
-function cadastrarDinossauro() {
-    let NomeLido = document.getElementById("nome").value
-    let AlturaLida = document.getElementById("altura").value
+// document.getElementById('dino').innerHTML = `Nome: ${dinossauros[0].nome}; Altura: ${dinossauros[0].altura}`
+
+function cadastrarDino(){
+    let nomeLido = document.getElementById('inputNome').value
+    let alturaLida = Number(document.getElementById('inputAltura').value)
 
     const dino = {
-        nome: NomeLido,
-        altura: AlturaLida
+        nome: nomeLido,
+        altura: alturaLida
     }
     dinossauros.push(dino)
-    console.log(dinossauros)
-    document.getElementById("nome").value = ""
-    document.getElementById("altura").value = ""
-    document.getElementById("nome").focus()
+
+    console.log(dinossauros);
+    
+    limparForm()
+    alert("Dino cadastrado com sucesso!")
+
+    mostrarTodosDinos()
+    
+    
 }
 
-function mostrarTodososDinos(){
+function limparForm(){
+    document.getElementById('inputNome').value = ''
+    document.getElementById('inputAltura').value = ''
+    // document.getElementById('inputPesoOuMassa').value = ''
+    
+    document.getElementById('inputNome').focus()
+}
 
+function mostrarTodosDinos(){
+    document.getElementById('listaDinos').innerHTML = '' 
     for(let i=0; i<dinossauros.length ; i++){
-        console.log(i)
-document.getElementById('painelDino').innerHTML +=`  Nome: ${dinossauros[0].nome} altura: ${dinossauros[0].altura}`
-    }}
+        // console.log(i);
+        document.getElementById('listaDinos').innerHTML += `
+            <div class='card'>
+                <h3>${dinossauros[i].nome} </h3>
+                <p>Altura: ${dinossauros[i].altura}</p>
+            </div>
+        `
+        
+    }
+}
+
+function lancarMeteoro(){
+    dinossauros.length = 0
+    mostrarTodosDinos()
+}
+
+function navegarParaDois(){
+    // alert('Tchau')
+
+    let nome = prompt("User:")
+    let senha = prompt("Senha: ")
+
+    if(nome == "admin" && senha == "admin"){
+        window.location.href = 'dois.html'
+    }else{
+        alert("Você está tentando invadir, a polícia tá chegando!!")
+    }
+
+}
 
     
