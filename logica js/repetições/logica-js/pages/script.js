e9d66fdba8f5fea2bba3a840cb672b3521c95258
vitor2008 <Vitor_tonello@gmail.com>
2025-11-11 22:09:30 -0300

    aula do Lucigol

---
+++ logica js/repetições/logica-js/pages/script.js
@@ -0,0 +1,16 @@
+// arquivo serviços
+
+const serviçoWeb = document.querySelector('#serviços-web')
+
+const showButton = document.querySelector('#show')
+
+const hideButton = document.querySelector('#hide')
+
+showButton.addEventListener('click', () => {
+    
+}
+
+
+    showButton.style.display =("click",() => {
+        serviçoWeb.classList.add("show")
+}
